623cd3f83ce18359369464cf09a301745a334a6f
Giulia Cossutti <giulia.cossutti@cern.ch>
2026-03-12 17:32:45 +0000

    confmatrices and vi matrices

--- plotter/plot_classes/vertex_plot_9labels.py
+++ plotter/plot_classes/vertex_plot_9labels.py
@@ -136,8 +136,11 @@ class VertexPlotBase(PlotBase):
             is_Disp = self.config.is_Disp
 
             # extract jet information
-            ds_jet = hdf_file['jets']
-            #ds_jet = hdf_file['jets'][:100000] # only if too many to load in memory
+            if is_Disp == 0:
+               ds_jet = hdf_file['jets']
+            elif is_Disp == 1:
+               ds_jet = hdf_file['jets'][:100000] # only if too many to load in memory
+             
             keys_list = list(ds_jet.dtype.fields.keys())
 
             # search for which key contains the probability of being displaced
@@ -163,9 +166,12 @@ class VertexPlotBase(PlotBase):
             print("about to extract ds_tfj info")
 
             # extract track information
-            ds_tfj = hdf_file[sample.df_name][idx]
-            #ds_tfj = hdf_file[sample.df_name][:100000]  # only if too many to load in memory
-            #ds_tfj = ds_tfj[idx]
+            if is_Disp == 0:
+               ds_tfj = hdf_file[sample.df_name]
+            elif is_Disp == 1:
+               ds_tfj = hdf_file[sample.df_name][:100000]  # only if too many to load in memory
+             
+            ds_tfj = ds_tfj[idx]
 
             ds_tfj_jet = ds_tfj[jet_num]  # Load the entire jet_num row once into memory
             
